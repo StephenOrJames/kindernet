11d10b2ddffa15ef35ad2fab4166635d9afe04ad
Smayan Daruka <smayandaruka@gmail.com>
2016-09-17 22:17:45 -0400

    Update replacements.py

--- kindernet/replacements.py
+++ kindernet/replacements.py
@@ -1,6 +1,11 @@
 REPLACEMENTS = {
     "bad": "good",
     "hate": "love"
+    "hard": "easy"
+    "rude": "nice"
+    "unfriendly": "friendly"
+    "misbehave": "behave"
+    "sucks": "rocks"
 }
 
 REPLACEMENTS.update({v: k for k, v in REPLACEMENTS.items()})
